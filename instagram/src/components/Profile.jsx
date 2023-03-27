b4d5e8a725010a999b4affd92a0b99861a7337c9
Vibhabakkam <vibhabakkam22@gmail.com>
2023-03-27 20:00:53 +0530

    Update Profile.jsx

--- instagram/src/components/Profile.jsx
+++ instagram/src/components/Profile.jsx
@@ -1,25 +1,47 @@
 import "./Components.css";
 import Sidebar from "./Sidebar";
 import Footer from "./Footer";
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 function Profile() {
+
+    const [post, setPost] = useState({
+        caption: "",
+        image: "",
+        username: "",
+        useremail: "",
+    });
+
+    useEffect(() => {
+        setUserInfo(JSON.parse(localStorage.getItem("instaCurrentUser")));
+        const userPostsFromLs = JSON.parse(localStorage.getItem("instaPost")) || [];
+        const filteredUserPosts = userPostsFromLs.filter(post => post.useremail === userInfo?.currentEmail);
+        setUserPosts(filteredUserPosts);
+    }, [setPost]);
+    const [userPosts, setUserPosts] = useState([]);
+    const [userInfo, setUserInfo] = useState(JSON.parse(localStorage.getItem("instaCurrentUser")));
+
+    useEffect(() => {
+        const userPostsFromLs = JSON.parse(localStorage.getItem("instaPost")) || [];
+        const filteredUserPosts = userPostsFromLs.filter(post => post.useremail === userInfo?.currentEmail);
+        setUserPosts(filteredUserPosts);
+    }, [setPost, userInfo]);
 
     return (
         <>
             <div class="profile-main">
-
-                <div class = "profile-Sidebar">
-                    <Sidebar/>
+                <div class="profile-Sidebar">
+                    <Sidebar />
                 </div>
-
                 <div class="profile">
 
                     <div class="profile-header">
                         <div class="profile-image">
-                            <img src="https://media.licdn.com/dms/image/C4E03AQGvXlEYY08elQ/profile-displayphoto-shrink_400_400/0/1631716060825?e=1684972800&v=beta&t=8UWC-9rjY-sC1lgc4Z2GR4XX_IlZpFl6Vps6uZYgvl0" alt="imge" />
+                            <img src="https://media.istockphoto.com/id/1447162783/photo/aligned-fishing-boats-in-the-sunset.jpg?b=1&s=170667a&w=0&k=20&c=Im3LT7-bWuXaNXfnGuTXc8leNYojrnwtDu5ohiUwvXo=" alt="imge" />
                         </div>
                         <div class="profile-info">
-                            <h1 class="profile-username">VibhaBakkam <button>Edit Profile</button> <i class="fa-solid fa-gear"></i></h1>
+                            <h1 class="profile-username">{userInfo && userInfo["currentUserName"]} <button>Edit Profile</button> <i class="fa-solid fa-gear"></i></h1>
                             <div class="profile-stats">
                                 <div class="profile-stat">
                                     <span class="profile-stat-count">1,234</span> posts
@@ -32,18 +54,23 @@ function Profile() {
                                 </div>
                             </div>
                             <div class="profile-bio">This is my bio. <br />
-                                VibhaBakkam <br />
-                                From - Mangoan
-
+                            This is heaven on earth.
                             </div>
                         </div>
                     </div>
                     <div class="profile-posts">
-
+                        {userPosts.map(post => (
+                            <div id="map-Post"  key={post.id}>
+                                <img src={post.image} alt="post" />
+                                <i class="fa-solid fa-heart"></i><i class="fa-solid fa-share-nodes"></i> <i class="fa-solid fa-comment"></i>
+                                <h2>{post.caption}</h2>
+                            </div>
+                        ))}
                     </div>
+
                 </div>
             </div>
-            <Footer/>
+            <Footer />
         </>
     )
 
